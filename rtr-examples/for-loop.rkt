dfe1eb2a023ac0baed2a2f52e6b2e4c4eb445c0f
Andrew Kent <andmkent@indiana.edu>
2016-02-13 12:34:54 -0500

    tweaks

--- rtr-examples/for-loop.rkt
+++ rtr-examples/for-loop.rkt
@@ -7,3 +7,19 @@
 (define (sum-vector v)
   (for/sum ([i (in-range (vector-length v))])
     (safe-vector-ref v i)))
+
+;; for loops expand into recursive functions
+;; (as can be seen with the Macro Stepper above)
+;; and our heuristic of guessing 'Index' for
+;; variables used for vector accesses works well
+;; for some loops (like the above) but not for
+;; others (e.g. like if the iteration is toward
+;; zero, since the last case i will be -1):
+
+;; does not typecheck with our simple heuristic
+#|
+(: rev-sum-vector : (Vectorof Integer) -> Integer)
+(define (rev-sum-vector v)
+  (for/sum ([i (in-range (- (vector-length v) 1) 0 -1)])
+    (safe-vector-ref v i)))
+|#
